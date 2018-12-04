e9f2c9f0116514758ac847eb97b77fff3a95d0e2
Dmytro <pokintelica@ukr.net>
2018-12-04 22:05:58 +0200

    http

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit , Input} from '@angular/core';
-import { ActivatedRoute, ActivatedRoute } from '@angular/router';
+import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { HeroService } from '../hero.service';
@@ -24,6 +24,11 @@ export class HeroDetailComponent implements OnInit {
     this.getHero();
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+  
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
